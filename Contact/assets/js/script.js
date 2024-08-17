6a051fbf5c18ab70009a3da4477f541a7c2c0af1
Janvi023kakadiya <kakadiyajanvi83@gmail.com>
2024-08-17 21:29:44 +0530

    first commit

---
+++ Contact/assets/js/script.js
@@ -0,0 +1,8 @@
+
+   
+//     $(document).ready(function) () {
+//     $('[data-aos^="fade"].aos-animate').css({
+//         'opacity': '1',
+//         'transform': 'translateZ(0)'
+//     });
+// }
